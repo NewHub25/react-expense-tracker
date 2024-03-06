a87986e7e88a5ccffc968aabd8ed8aaedc262bf1
Terry <master_correo144@hotmail.com>
2024-03-06 00:12:40 -0500

    ADD constants.js

--- src/context/global-context.jsx
+++ src/context/global-context.jsx
@@ -1,12 +1,15 @@
 import { createContext, useReducer } from "react";
 import appReducer from "../logic/app-reducer";
+import { initialState } from "../logic/constants";
 
 export const Context = createContext(null);
 
 export const GlobalProvider = ({ children }) => {
-  const [state, setState] = useReducer(appReducer, []);
+  const [state, dispatch] = useReducer(appReducer, initialState);
 
   return (
-    <Context.Provider value={{ state, setState }}>{children}</Context.Provider>
+    <Context.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </Context.Provider>
   );
 };
